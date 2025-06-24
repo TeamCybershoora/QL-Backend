896515481a5dc448936d4dee8ea28a6601582208
Tushar-Kumar12 <tk2004kumar@gmail.com>
2025-06-24 23:56:56 +0530

    Added frontend URL to API response

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT ;
 connect();
 
 app.use(cors({
-  origin: 'http://localhost:3000',  
+  origin: 'https://ql-frontend.vercel.app',  
   credentials: true,                 
 }));
 
